1b58ee593e46e17ba4b3384057e0fba1eb224824
kyseaton <kyles@jfmeng.com>
2016-02-16 07:51:34 -0500

    for comparison

--- NewBTASProto/DataSet1.cs
+++ NewBTASProto/DataSet1.cs
@@ -7,6 +7,21 @@ namespace NewBTASProto
 namespace NewBTASProto
 {
 }
+namespace NewBTASProto
+{
+}
+namespace NewBTASProto
+{
+}
+namespace NewBTASProto
+{
+}
+namespace NewBTASProto
+{
+}
+namespace NewBTASProto
+{
+}
 public partial class DataSet1 {
     partial class WOSumSetDataTable
     {
--- NewBTASProto/GlobalVars.cs
+++ NewBTASProto/GlobalVars.cs
@@ -85,12 +85,12 @@ namespace NewBTASProto
         /// <summary>
         /// this string holds the programversion text
         /// </summary>
-        public static string programVersion = "6.0.0.100";
+        public static string programVersion = "6.0.1";
 
         /// <summary>
         /// this string holds the program publish date
         /// </summary>
-        public static string programPubDate = "Feb 2 2016";
+        public static string programPubDate = "Feb 8 2016";
 
         /// <summary>
         /// this is where we hold our notification service settings...
